b8a15bd13931227003c6de91e5478468b2c307a6
Jesus Cuesta Arza <Jesus Cuesta Arza>
2017-03-29 20:54:48 +0200

    Anidacion de componentes con heroes

--- hero-world/src/index.js
+++ hero-world/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Segundo from './Segundo';
 import './index.css';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+ReactDOM.render(
+  <Segundo />,
+  document.getElementById('segundo')
+);
+
